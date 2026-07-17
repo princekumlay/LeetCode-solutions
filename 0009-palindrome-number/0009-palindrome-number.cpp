6481e5977a7d75f286152b39a63ddc9c4df1c95b
Kailash Kumlay <princekumlay10@gmail.com>
2026-07-17 17:17:58 +0530

    Time: 4 ms (31.38%), Space: 8.5 MB (92.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+        //reverting the number
+        int revertedNumber = 0;
+        while(x > revertedNumber){
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x = x / 10;
+        }
+        
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
