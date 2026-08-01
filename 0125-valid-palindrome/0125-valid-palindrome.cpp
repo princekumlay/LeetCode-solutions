fc5091fb2ac14e640df36e4ad470d9a5bfecf7f6
Kailash Kumlay <princekumlay10@gmail.com>
2026-08-01 16:40:33 +0530

    Time: 4 ms (14.25%), Space: 9.7 MB (94.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string result;
-        for(char &c : s){
-            if((c >= 'A' && c <= 'Z')){
-                result.push_back(c + 32);
-            }
-            else if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
-                result += c;
-            }
-        }
+        // string result;
+        // for(char &c : s){
+        //     if((c >= 'A' && c <= 'Z')){
+        //         result.push_back(c + 32);
+        //     }
+        //     else if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
+        //         result += c;
+        //     }
+        // }
 
-        int i = 0, j = result.length() - 1;
+        // int i = 0, j = result.length() - 1;
+        // while(i < j){
+        //     if(result[i] != result[j]){
+        //         return false;
+        //     }
+        //    i++;
+        //    j--;
+        // }
+
+        //second approach(two pointers approach) for better space complexity
+        int i = 0, j = s.length() - 1;
         while(i < j){
-            if(result[i] != result[j]){
+
+            //skiping non alphanumeric character
+            while(i < j && !isalnum(s[i])){
+                i++;
+            }
+            while(i < j && !isalnum(s[j])){
+                j--;
+            }
+
+            //comparing lowercase characters
+            if(tolower(s[i]) != tolower(s[j])){
                 return false;
             }
-           i++;
-           j--;
+            i++;
+            j--;
         }
 
         return true;
