70b978fb57bec0061c09d5b5e0d5dd42bf413faf
Kailash Kumlay <princekumlay10@gmail.com>
2026-08-21 21:11:42 +0530

    Time: 67 ms (66.15%), Space: 111.4 MB (41.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+        //brute force approach
+        // for(int i = 0; i < nums.size(); i++){
+        //     for(int j = i + 1; j < nums.size(); j++){
+        //         if(nums[i] == nums[j]) return true;
+        //     }
+        // }
+
+        //optimized approach using hash table
         unordered_set<int> seen;
         for(int num: nums){
-            if(seen.count(num)){
-                return true;
-            }
+            if(seen.find(num) != seen.end()) return true;
             seen.insert(num);
         }
         return false;
