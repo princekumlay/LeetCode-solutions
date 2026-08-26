1234445881c1e98e77f11ea390e3649ba7a615e1
Kailash Kumlay <princekumlay10@gmail.com>
2026-08-26 17:51:12 +0530

    Time: 292 ms (16.42%), Space: 81.5 MB (9.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,30 +6,30 @@ public:
         int left = 0;
 
         //better with unordered set
-        // unordered_set<char> st;
-        // for(int right = 0; right < n; right++){
-
-        //     //shrink window until the duplicate is removed
-        //     while(st.count(s[right])){
-        //         st.erase(s[left]);
-        //         left++;
-        //     }
-
-        //     //insert current character and update length
-        //     st.insert(s[right]);
-        //     length = max(length, right - left + 1);
-        // }
-
-        //optimized with last_seen map
-        unordered_map<char, int> last_seen;
+        unordered_set<char> st;
         for(int right = 0; right < n; right++){
-            if(last_seen.count(s[right]) && last_seen[s[right]] >= left){
-                left = last_seen[s[right]] + 1;
+
+            //shrink window until the duplicate is removed
+            while(st.count(s[right])){
+                st.erase(s[left]);
+                left++;
             }
 
-            last_seen[s[right]] = right;
+            //insert current character and update length
+            st.insert(s[right]);
             length = max(length, right - left + 1);
         }
+
+        //optimized with last_seen map
+        // unordered_map<char, int> last_seen;
+        // for(int right = 0; right < n; right++){
+        //     if(last_seen.count(s[right]) && last_seen[s[right]] >= left){
+        //         left = last_seen[s[right]] + 1;
+        //     }
+
+        //     last_seen[s[right]] = right;
+        //     length = max(length, right - left + 1);
+        // }
 
         return length;
     }
