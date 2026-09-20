2f2253eed240284a8e91cc29328188564cdf2067
Kailash Kumlay <princekumlay10@gmail.com>
2026-09-20 22:23:32 +0530

    Time: 8 ms (18.32%), Space: 12.6 MB (27.73%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        //pair of value and corresponding roman symbol
+        vector<pair<int, string>> romanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+            {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"},
+            {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+
+        string roman = "";
+        for(auto& [val, symb] : romanMap){
+            while(num >= val){
+                roman += symb;
+                num -= val;
+            }
+        }
+
+        return roman;
+    }
+};
